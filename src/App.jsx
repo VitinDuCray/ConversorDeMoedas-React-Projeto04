402b9751052648f6ab10513c56b4cbf64ef70252
Victor Hugo <161778672+VitinDuCray@users.noreply.github.com>
2024-11-07 20:52:44 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ConversorDeMoedas from './components/ConversorDeMoedas'
+
+function App() {
+
+
+  return (
+    <>
+      <ConversorDeMoedas/>
+    </>
+  )
+}
+
+export default App
